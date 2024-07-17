644b89054d9cc27b8654a1a8d047da96df83b354
GGufficiale <germangranovskii@mail.ru>
2024-07-18 01:05:04 +0300

    Заливаем старые проекты к уроку 20.2

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    if request.method == 'POST':
+        """Метод для приема инфы с фронтэнда и ее вывода в консоль"""
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    return render(request, 'main/index.html')
