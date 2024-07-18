98dfa42df7567cbcb74d30accef4def8196d77b0
GGufficiale <germangranovskii@mail.ru>
2024-07-18 17:02:46 +0300

    Добавил css & js файлы

--- main/views.py
+++ main/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    if request.method == 'POST':
-        """Метод для приема инфы с фронтэнда и ее вывода в консоль"""
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
+    student_list = Student.objects.all()
+    context = {
+        'object_list': student_list
+    }
     return render(request, 'main/index.html')
+
+# def index(request):
+#     if request.method == 'POST':
+#         """Метод для приема инфы с фронтэнда и ее вывода в консоль"""
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         print(f'{name} ({email}): {message}')
+#     return render(request, 'main/index.html')
