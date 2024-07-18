20ef6402129e7890ba5759ab62e394597b112ed5
GGufficiale <161942804+GGufficiale@users.noreply.github.com>
2024-07-18 17:37:30 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 10203040,
+        'PASSWORD': ,
         'PORT': 5432
     }
 }
