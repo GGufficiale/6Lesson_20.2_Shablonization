443d26946431e3aa6a738c54a9ec2863946bd40e
GGufficiale <germangranovskii@mail.ru>
2024-07-18 18:45:21 +0300

    Добавлены подшаблоны и базовые шаблоны

--- main/views.py
+++ main/views.py
@@ -5,15 +5,22 @@ from main.models import Student
 def index(request):
     student_list = Student.objects.all()
     context = {
-        'object_list': student_list
+        'object_list': student_list,
+        'title': 'Главная'
     }
     return render(request, 'main/index.html', context)
 
-# def index(request):
-#     if request.method == 'POST':
-#         """Метод для приема инфы с фронтэнда и ее вывода в консоль"""
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         print(f'{name} ({email}): {message}')
-#     return render(request, 'main/index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        """Метод для приема инфы с фронтэнда и ее вывода в консоль"""
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+
+        context = {
+            'title': 'Контакты'
+        }
+
+    return render(request, 'main/contact.html', context)
